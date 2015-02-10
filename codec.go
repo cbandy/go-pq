93078add2bf64610ce8cfc21a719c698154ea89e
Chris Bandy <bandy.chris@gmail.com>
2015-02-10 06:58:43 +0000

    Function to decode timestamptz to fixed offsets

--- codec.go
+++ codec.go
@@ -5,6 +5,7 @@ import (
 	"encoding/hex"
 	"fmt"
 	"strconv"
+	"time"
 )
 
 // Decode a byte slice from the bytea "escape" format.
@@ -53,6 +54,82 @@ func decodeByteaHex(s []byte) []byte {
 		errorf(err.Error())
 	}
 	return result
+}
+
+// Decode a Time from the "ISO" format.
+func decodeTimestamptzISO(src []byte) time.Time {
+	atoi := func(s []byte) (result int) {
+		for i := 0; i < len(s); i++ {
+			result = result*10 + int(s[i]-'0')
+		}
+		return
+	}
+
+	sepYearMonth := bytes.IndexByte(src, '-')
+	year := atoi(src[:sepYearMonth])
+	src = src[sepYearMonth:]
+
+	// Skips a separator and converts two digits
+	nextTwoDigits := func() (result int) {
+		result = atoi(src[1:3])
+		src = src[3:]
+		return
+	}
+
+	month := nextTwoDigits()
+	day := nextTwoDigits()
+	hour := nextTwoDigits()
+	minute := nextTwoDigits()
+	second := nextTwoDigits()
+	nanosecond, offset := 0, 0
+
+	// Time before current era is suffixed with BC
+	if src[len(src)-1] == 'C' {
+		// Negate the year and add one.
+		// See http://www.postgresql.org/docs/current/static/datetime-input-rules.html
+		year = 1 - year
+
+		// Strip " BC"
+		src = src[:len(src)-3]
+	}
+
+	// Offset from UTC is formatted ±hh[:mm[:ss]]
+	switch {
+	case len(src) > 6 && src[len(src)-6] == ':':
+		offset += atoi(src[len(src)-2:])
+		src = src[:len(src)-3]
+		fallthrough
+
+	case len(src) > 3 && src[len(src)-3] == ':':
+		offset += 60 * atoi(src[len(src)-2:])
+		src = src[:len(src)-3]
+		fallthrough
+
+	default:
+		offset += 3600 * atoi(src[len(src)-2:])
+		if src[len(src)-3] == '-' {
+			offset = -offset
+		}
+		src = src[:len(src)-3]
+	}
+
+	// Fractional seconds
+	if len(src) > 1 {
+		// Skip fraction separator
+		i := 1
+		for ; i < len(src); i++ {
+			nanosecond = nanosecond*10 + int(src[i]-'0')
+		}
+		// Scale to nanosecnds
+		for ; i < 10; i++ {
+			nanosecond *= 10
+		}
+	}
+
+	return time.Date(
+		year, time.Month(month), day,
+		hour, minute, second, nanosecond,
+		globalLocationCache.getLocation(offset))
 }
 
 // Encode a byte slice to the bytea "escape" format.
